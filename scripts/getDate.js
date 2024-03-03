21c4af7a8a2398abbee182e98b7e21c00a317c05
andreanat <andrea.nmb13@gmail.com>
2024-03-02 22:30:29 -0700

    aa

---
+++ scripts/getDate.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+
+document.querySelector('footer p:first-of-type').textContent += ` ${currentYear}`;
+
+const lastModifiedDate = new Date(document.lastModified);
+
+document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate.toDateString()}`;
